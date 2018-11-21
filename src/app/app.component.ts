430d3f7eebc4f3af2ffa8a6d8507308fc08d338a
Виктор Фамилия <shark_vik@inbox.ru>
2018-11-21 22:02:55 +0300

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nvm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'nvm';
+}
