d6ce32dad9fd5b38b2f282bd57b084b4e682218c
Виктор Фамилия <shark_vik@inbox.ru>
2018-11-28 14:09:58 +0300

    small fix

--- projects/nvm-cache/src/lib/models/nvm-subject.ts
+++ projects/nvm-cache/src/lib/models/nvm-subject.ts
@@ -5,60 +5,59 @@ import isNil from 'lodash/isNil';
 export class NvmSubject<T> extends ReplaySubject<T> {
 	private _action: () => Observable<T>;
 	private _lastValue: T;
-	private _tempSubject: Subject<T>;
+	private _tempSubjects: Subject<T>[] = [];
 	private _onRefresh = debounce(() => {
-		this._action()
-			.subscribe((data: T) => {
-				this.next(data);
-				this._emitTempSubject();
-			});
+		this._action().subscribe((data: T) => {
+			this.next(data);
+			this._emitTempSubject();
+		});
 	}, 300);
 
 	constructor(action: () => Observable<T>) {
 		super(1);
 		this._action = action;
-		this.subscribe((data: T) => this._lastValue = data);
+		this.subscribe((data: T) => (this._lastValue = data));
 	}
 
 	public refresh(): Observable<T> {
-		if (isNil(this._tempSubject)) {
-			this._tempSubject = new Subject<T>();
-		}
+		const subj = new Subject<T>();
+		this._tempSubjects.push(subj);
 		this._onRefresh();
-		return this._tempSubject;
+		return subj;
 	}
 
 	public update(data: T): Observable<T> {
-		return new Observable<T>((s) => {
+		return new Observable<T>(s => {
 			this.next(data);
 			this._emit(s);
 		});
 	}
 
 	public getOnce(): Observable<T> {
-		return new Observable<T>((s) => {
+		return new Observable<T>(s => {
 			if (!isNil(this._lastValue)) {
 				this._emit(s);
 				return;
 			}
-			const subs = this.asObservable()
-				.subscribe(() => {
-					this._emit(s);
-					if (!subs.closed) {
-						subs.unsubscribe();
-					}
-				});
+			const subs = this.asObservable().subscribe(() => {
+				this._emit(s);
+				if (!subs.closed) {
+					subs.unsubscribe();
+				}
+			});
 		});
 	}
 
 	private _emit = (s: Subscriber<T>, data?: T): void => {
 		s.next(data || this._lastValue);
 		s.complete();
-	}
+	};
 
 	private _emitTempSubject(): void {
-		this._tempSubject.next(this._lastValue);
-		this._tempSubject.complete();
-		this._tempSubject = null;
+		this._tempSubjects.forEach(s => {
+			s.next(this._lastValue);
+			s.complete();
+		});
+		this._tempSubjects = [];
 	}
 }
